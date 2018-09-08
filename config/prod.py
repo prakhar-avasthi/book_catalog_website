783fbd1f880a2ae00b3788ccd47ed3e1a2d2785f
prakhar-avasthi <prakhar.avasthi@stonybrook.edu>
2018-09-07 21:34:49 -0400

    Production Server Configuration

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:q1w2e3r4@localhost/catlog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
