03962129a8ee19edb33b39dbe7b438d9c08d95cb
prakhar-avasthi <prakhar.avasthi@stonybrook.edu>
2018-09-07 21:18:19 -0400

    Error handling

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -46,3 +46,8 @@ def do_the_login():
 def log_out_user():
     logout_user()
     return redirect(url_for('authentication.do_the_login'))
+
+
+@at.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
