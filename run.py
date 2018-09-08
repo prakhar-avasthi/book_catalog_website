c2a35762a6e92a354ec9c2fc60fbc8a5eca3f779
prakhar-avasthi <prakhar.avasthi@stonybrook.edu>
2018-09-07 21:28:25 -0400

    Production Server Configuration

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 
 
 if __name__ == '__main__':
-    flask_app = create_app('dev')
+    flask_app = create_app('prod')
 
     with flask_app.app_context():
         db.create_all()
